c9bd20c2012960e6e90ffdca4da751db5b6ef26e
harutaka[はるたか］ <chuisamfung@gmail.com>
2026-07-24 13:43:53 +0800

    Update Heap.h

--- Heap.h
+++ Heap.h
@@ -13,3 +13,12 @@ struct Heap
 };
 
 int Parent(int Index);
+int LeftChild(int Index);
+int RightChild(int Index);
+void Swap(int *A, int *B);
+void HeapUp(struct Heap *pHeap, int nIndexNumber);
+void MaxHeapify(struct  Heap *pHeap, int IndexAt
+void BuildMaxHeap(struct Heap *pHeap, int NumeroNoIndex);
+void HeapSort(struct Heap *pHeap, int Ueda);
+int HeapMaximum(struct Heap pHeap);
+void HeapInsert(struct Heap *pHeap, int Index, int Numbero);
