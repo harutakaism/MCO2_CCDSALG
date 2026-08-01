bc45f687fcbd5d0a701679d3b4a7000b4dd2eaba
GeoffGeoffGeoffGeoff <geofoncommand@gmail.com>
2026-08-01 23:32:51 +0800

    Update GraphTraversals.c
    added BFS

--- GraphTraversals.c
+++ GraphTraversals.c
@@ -1,4 +1,4 @@
-#include "GraphTraversals.h" 
+#include "GraphTraversals.h"  
  
  int isVisited(char visited[][257],int visitedCount,char *name)
 {
@@ -54,6 +54,48 @@ void depthFirstSearch(GraphTag *graph, char *startName)
 
         if (!foundNext){
             Pop(&stack);
+        }
+    }
+}
+
+void breadthFirstSearch(GraphTag *graph, char *startName)
+{
+    struct Queue queue;
+    char visited[256][257];
+    int visitedCount = 0;
+
+    if (findVertex(graph, startName) == NULL){
+
+        return;
+    }
+
+    CreateQ(&queue);
+
+    Enqueue(&queue, startName);
+
+    strcpy(visited[visitedCount], startName);
+    visitedCount++;
+
+    while (!QueueEmpty(&queue))
+    {
+        VertexNodeTag *currVertex = findVertex(graph, QueueHead(&queue));
+        EdgeNodeTag *currEdge = currVertex->firstEdge;
+
+        printf("%s\n", QueueHead(&queue));
+        Dequeue(&queue);
+        
+
+        while(currEdge != NULL)
+        {
+            if (!isVisited(visited, visitedCount, currEdge->dest))
+            {
+                Enqueue(&queue, currEdge->dest);
+
+                strcpy(visited[visitedCount], currEdge->dest);
+                visitedCount++;
+            }
+
+            currEdge = currEdge->nextEdge;
         }
     }
 }
